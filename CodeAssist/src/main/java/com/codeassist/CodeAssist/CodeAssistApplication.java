a0823d94281c78c98035c84d44a5daad4008f5a4
Raivo Lapins <lapins.raivo@gmail.com>
2019-07-16 13:31:54 +0300

    Project backbone created

---
+++ CodeAssist/src/main/java/com/codeassist/CodeAssist/CodeAssistApplication.java
@@ -0,0 +1,13 @@
+package com.codeassist.CodeAssist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeAssistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeAssistApplication.class, args);
+	}
+
+}
