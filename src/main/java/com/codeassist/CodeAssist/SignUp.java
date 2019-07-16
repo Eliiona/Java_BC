ff35fe777b1f8d52d3787231740b3a533203c93f
Elina Rostoka <elina.rostoka@gmail.com>
2019-07-16 17:22:25 +0300

    this is madness

---
+++ src/main/java/com/codeassist/CodeAssist/SignUp.java
@@ -0,0 +1,30 @@
+package com.codeassist.CodeAssist;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/signup")
+public class SignUp extends HttpServlet {
+
+	public SignUp() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doPost(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+	
+}
