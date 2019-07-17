6f2fae0fd951d95724e8c77a653bd33a503cdf33
Emils Verins <emils.verins@gmail.com>
2019-07-17 12:18:06 +0300

    Fixed #31

--- src/main/java/com/codeassist/CodeAssist/Model/User.java
+++ src/main/java/com/codeassist/CodeAssist/Model/User.java
@@ -1,33 +1,72 @@
 package com.codeassist.CodeAssist.Model;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
 import javax.validation.constraints.Size;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
+@Entity
+@Table(name = "UserTable")
 public class User {
+	//variables
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "Id_user")
+	private int id_user;
+	
+	@Column(name = "BC_Code")
 	@NotNull
 	@Size(min = 4, max = 4)
-	private String id;
+	private String bc_code;
+	
+	@Column(name = "Name")
 	@NotNull
 	@Size(min = 3, max = 20)
 	private String name;
+	
+	@Column(name = "Surname")
 	@NotNull
 	@Size(min = 3, max = 20)
 	private String surname;
-	private ArrayList<String> issues;
+	
+	@OneToMany(mappedBy = "user")
+	private Collection<Issue> issuesForUser;
+	
+	@OneToMany(mappedBy = "user")
+	private Collection<Reply> replysForIssue;
+	
+	@Column(name = "Password")
 	@NotNull
 	@Size(min = 6, max = 20)
 	private String password;
-
-	public String getId() {
-		return id;
+	
+	//constructors
+	public User(String name, String surname, String password, String bc_code) {
+		setName(name);
+		setSurname(surname);
+		setPassword(password);
+		setBcCode(bc_code);
+	}
+	
+	public User() {		
+	}
+	
+	
+	//set and get methods
+	public String getBcCode() {
+		return bc_code;
 	}
 
-	public void setId(String id) {
-		this.id = id;
+	public void setBcCode(String bc_code) {
+		this.bc_code = bc_code;
 	}
 
 	public String getName() {
@@ -52,6 +91,10 @@ public class User {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public int getId() {
+		return id_user;
 	}
 
 }
