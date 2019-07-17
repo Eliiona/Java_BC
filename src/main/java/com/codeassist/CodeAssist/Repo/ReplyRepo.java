382d495336fa43b191cbc4949f843d2558434c68
Raivo Lapins <lapins.raivo@gmail.com>
2019-07-17 12:46:52 +0300

    ReplyRepo done

--- src/main/java/com/codeassist/CodeAssist/Repo/ReplyRepo.java
+++ src/main/java/com/codeassist/CodeAssist/Repo/ReplyRepo.java
@@ -1,9 +1,9 @@
-package Repo;
+package com.codeassist.CodeAssist.Repo;
 
 import org.springframework.data.repository.CrudRepository;
+
 import com.codeassist.CodeAssist.Model.Reply;
 
 public interface ReplyRepo extends CrudRepository<Reply, Integer> {
-
 
 }
