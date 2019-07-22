e83ce647bfe7c31a5ed7b0ce8fb17ffe16596321
Elina Rostoka <elina.rostoka@gmail.com>
2019-07-22 15:46:50 +0300

    merging all

--- src/main/java/com/codeassist/CodeAssist/Controllers/UserController.java
+++ src/main/java/com/codeassist/CodeAssist/Controllers/UserController.java
@@ -1,12 +1,9 @@
 package com.codeassist.CodeAssist.Controllers;
 
-<<<<<<< HEAD
+
 import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
-
-=======
->>>>>>> master
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
