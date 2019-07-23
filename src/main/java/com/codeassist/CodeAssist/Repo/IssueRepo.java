fa16103c4cb427c89546810834eaba8426b72570
Emils Verins <emils.verins@gmail.com>
2019-07-23 13:53:26 +0300

    sito uzmanigi mergo

--- src/main/java/com/codeassist/CodeAssist/Repo/IssueRepo.java
+++ src/main/java/com/codeassist/CodeAssist/Repo/IssueRepo.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 
 import org.springframework.data.repository.CrudRepository;
 
+import com.codeassist.CodeAssist.Model.Activity;
 import com.codeassist.CodeAssist.Model.Issue;
 import com.codeassist.CodeAssist.Model.User;
 
 public interface IssueRepo extends CrudRepository<Issue, Integer>{
 	ArrayList<Issue> findByUser(User user);
+	ArrayList<Issue> findByActivity(Activity activity);
 }
